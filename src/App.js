d5802cdee31dacf91f7a97a512859e5f3e4fd219
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-28 14:09:58 +0600

    complete tailwind setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my WebSite</h1>
+      <h1 className='text-6xl'>Welcome to my WebSite</h1>
     </div>
   );
 }
